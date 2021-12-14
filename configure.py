1da91b85fab0ee07482d13376ad26aa6b94ed0aa
ma5ter <ma5ter@mail.ru>
2021-12-14 16:20:04 +0300

    Update configure.py

--- configure.py
+++ configure.py
@@ -12,7 +12,7 @@ def main(args):
     build.export_cpath("include", ["pthreadpool.h"])
 
     with build.options(source_dir="src", extra_include_dirs="src", deps=build.deps.fxdiv):
-        sources = ["legacy-api.c", "portable-api.c"]
+        sources = ["legacy-api.c", "portable-api.c", "memory.c"]
         if build.target.is_emscripten:
             sources.append("shim.c")
         elif build.target.is_macos:
